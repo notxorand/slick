9e106a4cd628cee63d68831d6ad47b9a19bf92bb
notxorand <ewanretorokugbe@gmail.com>
2026-06-29 20:20:56 +0100

    feat: implement rotation-aware camera movement
    + calculate movement vectors relative to camera rotation
    + add support for rendering rotated textures
    + wrap movement in camera coordinate system

--- src/main.zig
+++ src/main.zig
@@ -20,6 +20,7 @@ pub fn main(init: std.process.Init) !void {
 
     const allocator = arena.allocator();
     var textures = try std.ArrayList(rl.Texture2D).initCapacity(allocator, 16);
+
     const image = try rl.Image.init("./assets/simple_s.png");
     for (0..16) |_| {
         const texture = try rl.Texture.fromImage(image);
@@ -28,12 +29,14 @@ pub fn main(init: std.process.Init) !void {
     }
 
     const screen_center = rl.Vector2{ .x = 480, .y = 280 };
+    var control_position = rl.Vector2{ .x = 0, .y = 0 };
     var camera = rl.Camera2D{
         .offset = screen_center,
-        .target = .{ .x = 0, .y = 0 },
+        .target = control_position,
         .rotation = 0,
         .zoom = 1,
     };
+    const texture = textures.items[0];
 
     var entities: std.ArrayList(slick.StackEntity) = .empty;
 
@@ -45,10 +48,42 @@ pub fn main(init: std.process.Init) !void {
 
     while (!rl.windowShouldClose()) {
         const speed = 2.0;
-        if (rl.isKeyDown(.w)) camera.target.y += speed;
-        if (rl.isKeyDown(.s)) camera.target.y -= speed;
-        if (rl.isKeyDown(.a)) camera.target.x += speed;
-        if (rl.isKeyDown(.d)) camera.target.x -= speed;
+
+        const camera_rad = camera.rotation * (std.math.pi / 180.0);
+        const cos_a = @cos(camera_rad);
+        const sin_a = @sin(camera_rad);
+        const forward = rl.Vector2{
+            .x = -sin_a,
+            .y = -cos_a,
+        };
+
+        const right = rl.Vector2{
+            .x = cos_a,
+            .y = -sin_a,
+        };
+
+        const frame_speed = speed;
+
+        if (rl.isKeyDown(.w)) {
+            control_position.x += forward.x * frame_speed;
+            control_position.y += forward.y * frame_speed;
+        }
+        if (rl.isKeyDown(.s)) {
+            control_position.x -= forward.x * frame_speed;
+            control_position.y -= forward.y * frame_speed;
+        }
+        if (rl.isKeyDown(.a)) {
+            control_position.x -= right.x * frame_speed;
+            control_position.y -= right.y * frame_speed;
+        }
+        if (rl.isKeyDown(.d)) {
+            control_position.x += right.x * frame_speed;
+            control_position.y += right.y * frame_speed;
+        }
+        {
+            camera.target.x = control_position.x;
+            camera.target.y = control_position.y;
+        }
         if (rl.isKeyDown(.q)) camera.rotation += speed;
         if (rl.isKeyDown(.e)) camera.rotation -= speed;
         if (rl.isKeyDown(.z)) object_rotation += speed;
@@ -56,7 +91,19 @@ pub fn main(init: std.process.Init) !void {
         rl.beginDrawing();
         defer rl.endDrawing();
         rl.clearBackground(rl.Color.init(128, 128, 128, 255));
+        rl.beginMode2D(camera);
 
+        rl.drawTexturePro(
+            texture,
+            .{ .x = 0, .y = 0, .width = @floatFromInt(texture.width), .height = @floatFromInt(texture.height) },
+            .{ .x = control_position.x, .y = control_position.y, .width = @as(f32, @floatFromInt(texture.width)), .height = @as(f32, @floatFromInt(texture.height)) },
+            .{ .x = @as(f32, @floatFromInt(texture.width)), .y = @as(f32, @floatFromInt(texture.height)) },
+            -camera.rotation,
+            rl.Color.white,
+        );
+        rl.endMode2D();
+
+        // TODO: sort billboard sprites as well
         std.mem.sort(slick.StackEntity, entities.items, SortCtx{ .camera = camera }, SortCtx.lessThan);
         for (entities.items) |entity| {
             entity.render(camera, object_rotation); // there's has to be a better way than passing object_rotation to every render call
