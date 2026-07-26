ff97de366d6b3234875123bafe0e12f9dae1b8e8
notxorand <ewanretorokugbe@gmail.com>
2026-07-26 19:12:11 +0100

    feat: add car2 sprite assets and adjust stack rendering offset
    + add 20 sprite frames for the car2 model
    + tweak vertical anchor point in spritestack to improve alignment

--- src/component/spritestack.zig
+++ src/component/spritestack.zig
@@ -13,7 +13,7 @@ pub fn render(self: SpriteStack, x: f32, y: f32, rotation: f32, scale: f32) rl.V
             texture,
             .{ .x = 0, .y = 0, .width = @floatFromInt(texture.width), .height = @floatFromInt(texture.height) },
             .{ .x = x, .y = y - @as(f32, @floatFromInt(i)) * self.stack_height, .width = @as(f32, @floatFromInt(texture.width)) * scale, .height = @as(f32, @floatFromInt(texture.height)) * scale },
-            .{ .x = @as(f32, @floatFromInt(texture.width)) * scale / 2, .y = @as(f32, @floatFromInt(texture.height)) * scale / 2 },
+            .{ .x = @as(f32, @floatFromInt(texture.width)) * scale / 2, .y = @as(f32, @floatFromInt(texture.height)) * scale / 2.5 },
             self.rotation + rotation,
             rl.Color.white,
         );
